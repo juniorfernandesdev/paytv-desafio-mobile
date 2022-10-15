2f258fc161a61477ade8f015dbf1655101f9a062
Junior Fernandes <72802121+waldisonjunior@users.noreply.github.com>
2022-10-15 11:01:05 -0300

    structuring project

---
+++ paytv-desafio-mobile/paytv-desafio-mobile/Modules/Home/ViewController/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  paytv-desafio-mobile
+//
+//  Created by Junior Fernandes on 15/10/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
